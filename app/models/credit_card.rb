f78e1b18641934ba22a947a94aea82cc23dcb59d
Erik <flyers_rock_87@hotmail.com>
2019-12-04 10:47:52 -0500

    fix typo in credit_card.rb; change initializer for stripe

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -15,6 +15,6 @@ class CreditCard < ApplicationRecord
 
   validates :digits, :presence => true
   validates :month, :presence => true, :numericality => { :greater_than_or_equal_to => 1, :less_than_or_equal_to => 12 }
-  validates :year, :presence => true, :numericality => { :greater_than_or_equal_to => Datetime.now.year }
+  validates :year, :presence => true, :numericality => { :greater_than_or_equal_to => DateTime.now.year }
 
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  publishable_key: Rails.application.secrets['stripe'][:publishable_key],
-  secret_key: Rails.application.secrets['stripe'][:secret_key]
+  publishable_key: Rails.env == "production" ? ENV["STRIPE_PUB_KEY"] : Rails.application.secrets['stripe'][:publishable_key],
+  secret_key: Rails.env == "production" ? ENV["STRIPE_SECRET_KEY"] : Rails.application.secrets['stripe'][:secret_key]
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
